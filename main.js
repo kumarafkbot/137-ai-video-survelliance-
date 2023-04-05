595f045be501d94d785aa1e1ebf85f0bf38fbf17
kumarafkbot <96719832+kumarafkbot@users.noreply.github.com>
2023-04-05 16:29:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+img="";
+status = "";
+objects=[];
 function setup() {
     canvas=createCanvas(300,200)
     canvas.center()
@@ -12,6 +15,26 @@ function preload() {
 
 function draw() {
     image(video ,0,0,200,200)  
+    if(status != "")
+{
+    objectDetector.detect(video , gotresults)
+
+    for(i = 0; i < objects.length; i++ )
+    {
+      document.getElementById("status").innerHTML = "Status : Object Detected";
+      document.getElementById("number_of_objects").innerHTML = "number of objects detected are : " + objects.length
+     r = random(255)
+     g = random(255)
+     b = random(255)
+      fill(r,g,b);
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x , objects[i].y);
+      noFill();
+      stroke(r,g,b);
+    rect(objects[i].x -30, objects[i].y, objects[i].width,objects[i].height); }
+    }    
+
+
 }
 
 function start() {
@@ -26,3 +49,20 @@ video.loop()
 video.speed(1)
 video.volume(0)
 }
+
+
+function gotresults(error , results) {
+    if (error) {
+        console.log(error)
+        
+    } else {
+        console.log(results)  
+        objects = results;
+    }
+}
+
+
+
+
+
+
